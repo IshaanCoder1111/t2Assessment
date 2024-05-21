6d9f0f1a9e73b42fb3bc11fe30e83d4e06f33b79
DEBPLAYZ <debarun.dey@education.nsw.gov.au>
2024-05-21 12:35:56 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,57 @@
+import os, time
+
+#Adding a clear function
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+#Loading Function
+loading = ["Loading👑", "Loading.👑", "Loading..👑"]
+def gameloading():
+    for i in loading:
+                print(i)
+                time.sleep(0.5)
+                clear()
+
+#Starting Game
+def startermenu():
+    clear()
+    print("Welcome To")
+    print("""
+  
+        _       _________ _        _______    _______  _______   _________          _______    _______  _______  _______ _________ _        _______ 
+        | \    /\\__   __/( (    /|(  ____ \  (  ___  )(  ____ \  \__   __/|\     /|(  ____ \  (  ____ \(  ___  )(  ____ \\__   __/( \      (  ____ \
+        |  \  / /   ) (   |  \  ( || (    \/  | (   ) || (    \/     ) (   | )   ( || (    \/  | (    \/| (   ) || (    \/   ) (   | (      | (    \/
+        |  (_/ /    | |   |   \ | || |        | |   | || (__         | |   | (___) || (__      | |      | (___) || (_____    | |   | |      | (__    
+        |   _ (     | |   | (\ \) || | ____   | |   | ||  __)        | |   |  ___  ||  __)     | |      |  ___  |(_____  )   | |   | |      |  __)   
+        |  ( \ \    | |   | | \   || | \_  )  | |   | || (           | |   | (   ) || (        | |      | (   ) |      ) |   | |   | |      | (      
+        |  /  \ \___) (___| )  \  || (___) |  | (___) || )           | |   | )   ( || (____/\  | (____/\| )   ( |/\____) |   | |   | (____/\| (____/\
+        |_/    \/\_______/|/    )_)(_______)  (_______)|/            )_(   |/     \|(_______/  (_______/|/     \|\_______)   )_(   (_______/(_______/
+                                                                                                                                                               
+          """)
+    input("Press Any Key To Continue!")
+    clear()
+
+def nameselection():
+    clear()
+    print("Choose a Fierce Name to Proceed!")
+    while True:
+        namechosen = input("Chosen Name(No More Than 10 Characters): ")
+        if 0 < len(namechosen) <= 10:
+            print(f"WELCOME TO KING OF THE CASTLE {namechosen.upper()}")
+            input("Press any key to continue")
+            gameloading()
+            break
+        elif len(namechosen) >= 10:
+            print("That name is too long!")
+            time.sleep(1.5)  
+        else:
+            print("Type a letter at least!")
+            time.sleep(1.5)
+    clear()
+
+
 class Item:
-    pass
+    def __init__
 
 
 item1 - Item()
