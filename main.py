63cde707019ee32d4600b2742ae697d53be071ac
Nabbank <nabethan.manovarnnan@education.nsw.gov.au>
2024-05-24 11:31:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -137,8 +137,8 @@ class Player:
     
      def movement(self, direction):
         directions = {
-            "north": (0, 1),
-            "south": (0, -1),
+            "north": (0, -1),
+            "south": (0, 1),
             "east": (1, 0),
             "west": (-1, 0)
         }
